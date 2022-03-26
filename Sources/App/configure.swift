ce728405206019dc38ad4a649ac2210d0a632893
kevinzhow <kevinchou.c@gmail.com>
2022-03-26 13:23:25 +0800

    feat(db): add db env

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -9,7 +9,7 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
+    app.databases.use(.sqlite(.file(Environment.get("DB_URL") ?? "db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateFeishuWebhook())
 
