4882af2f8b6e89b4bee98fb9c4677cdcbe9d168a
kevinzhow <kevinchou.c@gmail.com>
2022-03-28 14:56:48 +0800

    feat(docker): simpler env logic

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -10,7 +10,7 @@ public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
 
-    app.databases.use(.sqlite(.file(Environment.get("DB_URL") ?? "db.sqlite")), as: .sqlite)
+    app.databases.use(.sqlite(.file(Environment.get("DB_URL") ?? "/app/data/db.sqlite")), as: .sqlite)
 
     app.migrations.add(CreateFeishuWebhook())
 
@@ -18,13 +18,13 @@ public func configure(_ app: Application) throws {
     
     app.routes.defaultMaxBodySize = "500kb"
     
-    if let cert = Environment.get("TLS_CERT"), let key = Environment.get("TLS_KEY") {
+    // Check if user has ENABLE_TLS TLS_CERT and TLS_KEY enviroment
+    if let enableTLS = Environment.get("ENABLE_TLS"), enableTLS == "true" {
         try app.http.server.configuration.tlsConfiguration = .makeServerConfiguration(
-            certificateChain: NIOSSLCertificate.fromPEMFile(cert).map { .certificate($0) },
-            privateKey: .file(key)
+            certificateChain: NIOSSLCertificate.fromPEMFile(Environment.get("TLS_CERT") ?? "/app/certs/cert.pem").map { .certificate($0) },
+            privateKey: .file(Environment.get("TLS_KEY") ?? "/app/certs/key.pem")
         )
     }
-    
 
     // Admin
 
